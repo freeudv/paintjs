f81eafac67c2e38c450154c1ecaa6608abbf8335
Oleg Polyakov <olegpolyakov@outlook.com>
2017-08-24 19:00:43 +0300

    Init

---
+++ js/app.js
@@ -0,0 +1,67 @@
+class App {
+    constructor({ canvas, colorPalette, colorPicker }) {
+        this.canvas = canvas;
+        this.colorPalette = colorPalette;
+        this.colorPicker = colorPicker;
+
+        this.context = null;
+        this.isDrawing = false;
+
+        this.init();
+    }
+
+    init() {
+        this.context = this.canvas.getContext('2d');
+
+        this.canvas.addEventListener('mousedown', this.handleCanvasMousedown.bind(this));
+        this.canvas.addEventListener('mousemove', this.handleCanvasMousemove.bind(this));
+        this.canvas.addEventListener('mouseup', this.handleCanvasMouseup.bind(this));
+        this.canvas.addEventListener('mouseleave', this.handleCanvasMouseleave.bind(this));
+
+        this.colorPicker.onAdd = color => this.colorPalette.addColor(color);
+
+        document.querySelector('#new-color-button')
+            .addEventListener('click', this.handleNewColorButtonClick.bind(this));
+        document.querySelector('#clear-canvas-button')
+            .addEventListener('click', this.handleClearCanvasButton.bind(this));
+        document.querySelector('#brush-size-slider')
+            .addEventListener('change', this.handleBrushSizeChange.bind(this));
+    }
+
+    handleNewColorButtonClick() {
+        this.colorPicker.open();
+    }
+
+    handleClearCanvasButton() {
+        this.context.fillStyle = 'white';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    handleBrushSizeChange(event) {
+        this.context.lineWidth = Number(event.target.value);
+    }
+
+    handleCanvasMousedown(event) {
+        this.lastEvent = event;
+        this.isDrawing = true;
+    }
+
+    handleCanvasMousemove(event) {
+        if (this.isDrawing) {
+            this.context.beginPath();
+            this.context.moveTo(this.lastEvent.offsetX, this.lastEvent.offsetY);
+            this.context.lineTo(event.offsetX, event.offsetY);
+            this.context.strokeStyle = this.colorPalette.currentColor;
+            this.context.stroke();
+            this.lastEvent = event;
+        }
+    }
+
+    handleCanvasMouseup() {
+        this.isDrawing = false;
+    }
+
+    handleCanvasMouseleave() {
+        this.isDrawing = false;
+    }
+}
---
+++ js/color-picker.js
@@ -0,0 +1,51 @@
+class ColorPicker {
+    constructor({ element }) {
+        this.element = element;
+        this.previewElement = this.element.querySelector('.color-picker__preview');
+
+        this.color = {
+            red: 0,
+            green: 0,
+            blue: 0
+        };
+
+        this.onAdd = () => {};
+
+        this.init();
+    }
+
+    init() {
+        this.previewElement.style.backgroundColor = `rgb(${this.color.red},${this.color.green},${this.color.blue})`;
+        
+        this.element
+            .querySelector('.color-picker__close-button').addEventListener('click', this.close.bind(this));
+
+        this.element
+            .querySelector('.color-picker__add-button')
+            .addEventListener('click', this.handleAdd.bind(this));
+
+        this.element
+            .querySelectorAll('.color-picker__slider')
+            .forEach(slider => slider.addEventListener('change', this.handleChange.bind(this)));
+    }
+
+    handleChange(event) {
+        let slider = event.target;
+
+        this.color[slider.id] = Number(slider.value);
+        this.previewElement.style.backgroundColor = `rgb(${this.color.red},${this.color.green},${this.color.blue})`;
+    }
+
+    handleAdd(event) {
+        this.onAdd(this.color);
+        this.close();
+    }
+
+    open() {
+        this.element.style.display = 'block';
+    }
+
+    close(event) {
+        this.element.style.display = 'none';
+    }
+}
